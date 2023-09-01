9c0b19bffff88c009007f26dfb5106419aa76b31
godzzz <g0dzZz1lla@yandex.ru>
2023-09-01 18:35:34 +0400

    Tests moved to branch tests

--- Tests/EditMode/Stubs/NonExistentScriptableAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright © 2023 Nikolay Melnikov. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-using UnityEngine;
-
-namespace Depra.Assets.Tests.EditMode.Stubs
-{
-	internal sealed class NonExistentScriptableAsset : ScriptableObject { }
-}
--- Tests/PlayMode/Stubs/TestMonoAsset.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright © 2023 Nikolay Melnikov. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-using UnityEngine;
-
-namespace Depra.Assets.Tests.PlayMode.Stubs
-{
-	internal sealed class TestMonoAsset : MonoBehaviour { }
-}
