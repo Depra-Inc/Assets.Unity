492831a3bd2d22203a6d8bb4aaf6264f9561d640
godzzz <g0dzZz1lla@yandex.ru>
2023-09-26 01:02:43 +0400

    Cleaning

--- Runtime/Files.Database/AssetCatNotBeCreated.cs
+++ Runtime/Files.Database/AssetCatNotBeCreated.cs
@@ -8,7 +8,7 @@ namespace Depra.Assets.Runtime.Files.Database
 {
 	internal sealed class AssetCatNotBeCreated : Exception
 	{
-		private const string MESSAGE_FORMAT = "Asset {0} with type {1} can not be created!";
+		private const string MESSAGE_FORMAT = "Asset '{0}' with type '{1}' can not be created!";
 
 		public AssetCatNotBeCreated(MemberInfo assetType, string assetName) :
 			base(string.Format(MESSAGE_FORMAT, assetName, assetType.Name)) { }
